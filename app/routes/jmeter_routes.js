3d4cf19b814ab7b17cc75db26212b58d56a94674
nikitin_serg <nikitin.serg@sharp-dev.net>
2020-01-23 22:19:30 +0300

    save as json only

--- app/routes/jmeter_routes.js
+++ app/routes/jmeter_routes.js
@@ -5,6 +5,10 @@ var multer  = require('multer');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, `${appRoot}/newman/settings`)
+  },
+  filename: function (req, file, cb) {
+	console.log(file)
+    cb(null, file.originalname.split('.')[0]+'.json')
   }
 })
 var upload = multer(({ 
